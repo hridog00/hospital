f7ebfc7e16ed581f063e2b3258985218eb79c272
hridog00 <hridog00@estudiantes.unileon.es>
2018-01-15 12:07:40 +0100

    tercero

--- app/src/main/java/com/example/andorid/hospital/MainActivity.java
+++ app/src/main/java/com/example/andorid/hospital/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
