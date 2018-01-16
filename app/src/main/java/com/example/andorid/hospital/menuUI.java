3472cf52edad1c125890d5544810c8f6929cc58c
smeraa00 <sergiomerayo@gmail.com>
2018-01-16 13:56:34 +0100

    uno

--- app/src/main/java/com/example/andorid/hospital/menuUI.java
+++ app/src/main/java/com/example/andorid/hospital/menuUI.java
@@ -101,6 +101,8 @@ public class menuUI extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
+            Intent  medicacion_enfermero  = new Intent(getApplicationContext(), MedicacionEnfermeroUI.class);
+            startActivity(medicacion_enfermero);
 
 
         } else if (id == R.id.nav_slideshow) {
