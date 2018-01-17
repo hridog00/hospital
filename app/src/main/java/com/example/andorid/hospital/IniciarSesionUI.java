416128217fd05eae765785ded357871d8742299a
hridog00 <hridog00@estudiantes.unileon.es>
2018-01-17 13:05:50 +0100

    sesion

--- app/src/main/java/com/example/andorid/hospital/IniciarSesionUI.java
+++ app/src/main/java/com/example/andorid/hospital/IniciarSesionUI.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -33,6 +34,8 @@ public class IniciarSesionUI extends Activity {
                     menu.putExtra("Paciente",info );
                     startActivity(menu);
                 }else{
+                    TextView error = (TextView) findViewById(R.id.error);
+                    error.setVisibility(View.VISIBLE);
                     //mensaje de error
                 }
 
--- app/src/main/java/com/example/andorid/hospital/PrincipalController.java
+++ app/src/main/java/com/example/andorid/hospital/PrincipalController.java
@@ -5,4 +5,6 @@ package com.example.andorid.hospital;
  */
 
 public class PrincipalController {
+
+
 }
