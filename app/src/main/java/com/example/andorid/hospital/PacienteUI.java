982bb75ee78363ed60d49a19cae28465090bcad9
hridog00 <hridog00@estudiantes.unileon.es>
2018-01-17 11:48:19 +0100

    botonoes

--- app/src/main/java/com/example/andorid/hospital/PacienteUI.java
+++ app/src/main/java/com/example/andorid/hospital/PacienteUI.java
@@ -1,7 +1,10 @@
 package com.example.andorid.hospital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PacienteUI extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class PacienteUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paciente_ui);
+        Button medicacion = (Button) findViewById(R.id.button5);
+
+        medicacion.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //llamar a factoria
+                Intent med  = new Intent(getApplicationContext(), MedicacionEnfermeroUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(med);
+
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/andorid/hospital/menuUI.java
+++ app/src/main/java/com/example/andorid/hospital/menuUI.java
@@ -64,6 +64,7 @@ public class menuUI extends AppCompatActivity
 
             @Override
             public void onClick(View v) {
+
                 Intent  addPac  = new Intent(getApplicationContext(), AddPacienteUI.class);
 
                 //PrincipalUI p = new PrincipalUI();
@@ -108,9 +109,173 @@ public class menuUI extends AppCompatActivity
         });
 //hacer for con los pacientes
 
-        Button botonPaciente = (Button) findViewById(R.id.hab1);
+        Button hab1 = (Button) findViewById(R.id.hab1);
 
-        botonPaciente.setOnClickListener(new View.OnClickListener() {
+        hab1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab2 = (Button) findViewById(R.id.hab2);
+
+        hab2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab3 = (Button) findViewById(R.id.hab3);
+
+        hab3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab4 = (Button) findViewById(R.id.hab4);
+
+        hab4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab5 = (Button) findViewById(R.id.hab5);
+
+        hab5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab6 = (Button) findViewById(R.id.hab6);
+
+        hab6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        }); Button hab7 = (Button) findViewById(R.id.hab7);
+
+        hab7.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab8 = (Button) findViewById(R.id.hab8);
+
+        hab8.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab9 = (Button) findViewById(R.id.hab9);
+
+        hab9.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab10 = (Button) findViewById(R.id.hab10);
+
+        hab10.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab11 = (Button) findViewById(R.id.hab11);
+
+        hab11.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent  paciente  = new Intent(getApplicationContext(), PacienteUI.class);
+
+                //PrincipalUI p = new PrincipalUI();
+                //p.rellenar(info);
+                // Intent PrincipalUI  = new Intent(getApplicationContext(), menuUI.class);
+
+                startActivity(paciente);
+            }
+        });
+        Button hab12 = (Button) findViewById(R.id.hab12);
+
+        hab12.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
