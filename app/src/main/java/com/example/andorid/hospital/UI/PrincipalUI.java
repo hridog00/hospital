2aed951dfe600188c71ca371ab516707886d2330
hridog00 <hridog00@estudiantes.unileon.es>
2018-01-18 12:02:28 +0100

    sesion

--- app/src/main/java/com/example/andorid/hospital/UI/PrincipalUI.java
+++ app/src/main/java/com/example/andorid/hospital/UI/PrincipalUI.java
@@ -3,12 +3,13 @@ package com.example.andorid.hospital.UI;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.andorid.hospital.R;
 
-public class PrincipalUI extends Activity{
+public class PrincipalUI extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
