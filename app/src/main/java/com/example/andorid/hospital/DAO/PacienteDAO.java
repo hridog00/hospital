8b29be7c0463c3a85330facd832a79d3abf866be
hridog00 <hridog00@estudiantes.unileon.es>
2018-01-19 22:07:09 +0100

    no se

--- app/src/main/java/com/example/andorid/hospital/DAO/PacienteDAO.java
+++ app/src/main/java/com/example/andorid/hospital/DAO/PacienteDAO.java
@@ -21,6 +21,38 @@ public class PacienteDAO {
         Future<List> resultado = service.submit(new ConexionBD(addUsQSL));
         List res = resultado.get();
 
+        ExecutorService service1 = Executors.newFixedThreadPool(2);
+
+        String sql = "Select idUsuario FROM Usuario where username='P"+p.getDNI()+"'";
+        Future<List> resultado2 = service.submit(new ConexionBD(sql));
+        List res2 = resultado2.get();
+        System.out.println(res2.size());
+
+        String idUuario = ((List)res2.get(0)).get(0).toString();
+
+        String inserUsuario = "INSERT INTO Paciente ('idPaciente') VALUES('"+idUuario+"')";
+        Future<List> resultado3 = service.submit(new ConexionBD("INSERT INTO `mydb`.`Paciente` (`idPaciente`) VALUES ('"+idUuario+"');\n"));
+
+
+        String sqlHabitacion = "UPDATE `mydb`.`Habitacion` SET `idPacienteH`='"+idUuario+"' WHERE `NumeroHabitacion`='"+p.getHabitacionID()+"' and`PlantaH`='"+p.getPlanta()+"';";
+         resultado3 = service.submit(new ConexionBD(sqlHabitacion));
+
+        for(int i=0;i<p.getAlergias().size();i++) {
+            String sqlAlergias = "INSERT INTO `mydb`.`Alergias` (`idPaciente`, `Nombre`) VALUES ('" + idUuario + "', '" + p.getAlergias().get(i) + "');\n";
+            resultado3 = service.submit(new ConexionBD(sqlAlergias));
+
+        }
+
+        for(int i=0;i<p.getNecesidadesVH().size();i++){
+            String sqlVir = "INSERT INTO `mydb`.`NecesidadesVH` (`idPaciente`, `Nombre`) VALUES ('"+idUuario+"', '"+p.getNecesidadesVH().get(i)+"');";
+            resultado3 = service.submit(new ConexionBD(sqlVir));
+
+        }
+
+
+
+
+
 
 
 
