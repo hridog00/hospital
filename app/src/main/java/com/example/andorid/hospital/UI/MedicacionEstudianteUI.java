cf4e8f8904be56287441dcf8baff459692987d6d
hridog00 <hridog00@estudiantes.unileon.es>
2018-01-22 10:48:12 +0100

    vnc

--- app/src/main/java/com/example/andorid/hospital/UI/MedicacionEstudianteUI.java
+++ app/src/main/java/com/example/andorid/hospital/UI/MedicacionEstudianteUI.java
@@ -6,15 +6,54 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
+import com.example.andorid.hospital.Controllers.MedicacionController;
+import com.example.andorid.hospital.Medicacion;
 import com.example.andorid.hospital.R;
+
+import java.util.ArrayList;
 
 public class MedicacionEstudianteUI extends AppCompatActivity {
 
+    MedicacionController medicacionController = new MedicacionController();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicacion_estudiante_ui);
+
+        Bundle datos = this.getIntent().getExtras();
+
+        final int idPaciente = datos.getInt("idPaciente");
+
+        ArrayList<Medicacion> medicacions = new ArrayList<>();
+
+        try {
+            medicacions = medicacionController.getMedicacion(idPaciente);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<TextView> textviews = new ArrayList<>();
+        GridLayout grid = (GridLayout) findViewById(R.id.grid);
+
+        for (int i = 0; i < medicacions.size(); i++) {
+            // create a new textview
+            final TextView textv = new TextView(this);
+
+            // set some properties of rowTextView or something
+            textv.setText("Nombre: " + medicacions.get(i).getNombre() + " Dosis: " + medicacions.get(i).getDosis() + " Hora: " + medicacions.get(i).getHora());
+
+            // add the textview to the linearlayout
+
+            grid.addView(textv);
+
+            // save a reference to the textview for later
+            textviews.add(textv);
+        }
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
